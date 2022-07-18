06b6778fbc52b316fbbbbdd4a7d331d274061d45
SittikronNon <sittikron.lamulsab@gmail.com>
2022-07-19 03:40:04 +0700

    new Wave Spawn updated
    updating wavespawner
    -3 new enemies
    -new spawnrate and countdown timer

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -6,7 +6,7 @@ public class WaveSpawner : MonoBehaviour
 
     public static int EnemiesAlive = 0;
 
-    public Transform enemyPrefab;
+    public Wave[] waves;
 
     public Transform spawnPoint;
     
@@ -19,10 +19,15 @@ public class WaveSpawner : MonoBehaviour
 
     void Update()
     {
+        if(EnemiesAlive > 0)
+        {
+            return;
+        }
         if(countdown <= 0f)
         {
             StartCoroutine(Spawnwave());
             countdown = timeBetweenWaves;
+            return;
         }
 
         countdown -= Time.deltaTime;
@@ -35,20 +40,25 @@ public class WaveSpawner : MonoBehaviour
     
     IEnumerator Spawnwave()
     {
-        waveIndex++;
         PlayerStats.Rounds++;
-
-        for (int i = 0; i < waveIndex; i++)
+        Wave wave = waves[waveIndex];
+        for (int i = 0; i < wave.count; i++)
         {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.5f);
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(1f / wave.spawnRate);
         }
+        waveIndex++;
+
+        if(waveIndex == waves.Length)
+        {
+            Debug.Log("LEVEL WON!");
+        }    
         
     }
 
-    void SpawnEnemy()
+    void SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
         EnemiesAlive++;
     }
 }
