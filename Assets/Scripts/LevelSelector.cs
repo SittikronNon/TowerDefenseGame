0b6a01e5b85a6b1adefe617f3f1af944c0d6f651
SittikronNon <sittikron.lamulsab@gmail.com>
2022-07-19 19:31:31 +0700

    Update LevelSelector.cs

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelector : MonoBehaviour
 {
     public SceneFader fader;
+
+    public Button[] levelButtons;
+
+    void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = false;
+        }
+    }
 
     public void Select(string levelName)
     {
